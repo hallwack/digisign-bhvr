cb1c9950778b283b2b7820c3600c5a9c3c20a70b
Raihan Adam <hallwack.id@gmail.com>
2025-06-15 00:14:10 +0700

    chore(client): format vite.config.ts for consistency and readability

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,7 +10,7 @@ export default defineConfig({
       "@client": path.resolve(__dirname, "./src"),
       "@server": path.resolve(__dirname, "../server/src"),
       "@shared": path.resolve(__dirname, "../shared/src"),
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
